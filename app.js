d1e5a96568e9c8b69ee0879f4b8f9ae2f646fd8d
Louie <ljanc001@fiu.edu>
2020-08-18 12:16:29 -0400

    Updated mongoose database connection logic

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit')
 const { nanoid } = require('nanoid')
 const shortURL = require('./models/shortURL')
 
-mongoose.connect('mongodb://localhost/shortner', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URI || 'mongodb://localhost/shortner', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connection.on('connected', () => console.log('Connected to Database'))
 mongoose.connection.on('error', (err) => console.log(err))
 
